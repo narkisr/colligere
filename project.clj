6623265e69b85bc520f0ca5afaf4e87ed1aa00fb
ronen <narkisr@gmail.com>
2016-12-08 01:02:33 +1100

    processing a list of hosts and transforming xml to a sand hash

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "TBD"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [
+      [org.clojure/data.xml "0.0.8"]
+      [org.clojure/data.zip "0.1.2"]
       [org.clojure/clojure "1.8.0"]
-      [http-kit "2.2.0"] 
-      ])
+      [http-kit "2.2.0"]])
--- src/colligere/core.clj
+++ src/colligere/core.clj
@@ -1,26 +1,36 @@
 (ns colligere.core
-  (:require [org.httpkit.client :as http]) 
-  )
+  (:require 
+    [clojure.string :refer (join lower-case replace)]
+    [clojure.data.xml :refer (parse-str)] 
+    [clojure.zip :refer (xml-zip)]
+    [org.httpkit.client :as http]
+    [clojure.data.zip.xml :refer  [xml-> xml1-> attr attr= text]]))
 
-(def conf)
+(def conf (read-string  (slurp "config.edn")))
 
-(def login "https://" (conf :host) "/cgi/login.cgi")
-(def ipmi "https://" (conf :host) "/cgi/ipmi.cgi")
+(defn login [host] (str "https://" host "/cgi/login.cgi"))
+(defn ipmi [host] (str "https://" host "/cgi/ipmi.cgi"))
 
 
-(defn get-sid []
-  (let [{:keys [headers]} @(http/post login {:form-params auth :insecure? true})]
+(defn get-sid [host auth]
+  (let [{:keys [headers :as resp]} @(http/post (login host) {:form-params auth :insecure? true})]
     (second (re-find #",SID\=([a-z]*);" (:set-cookie headers)))))
 
-(defn server-health []
-  (let [sid (get-sid)
+(defn get-health [{:keys [host auth]}]
+  (let [sid (get-sid host auth)
         headers {"Cookie" (str "SID=" sid ";" " mainpage=health; subpage=servh_sensor")
                  "Accept" "application/json" }
         form-data {"SENSOR_INFO.XML" "(1,ff)"}
-        res  @(http/post ipmi {:headers headers :form-params form-data :insecure? true})
+        {:keys [body]}  @(http/post (ipmi host) {:headers headers :form-params form-data :insecure? true})
         ]
-    (clojure.pprint/pprint res)
-   )
-  )
+    (xml-zip (parse-str body))))
 
-(server-health)
+(defn norm-name [m] 
+  (-> m :NAME (replace " " "-") lower-case keyword))
+
+(defn server-health [host]
+  (map #(attr % (juxt norm-name :READING)) (xml-> (get-health host) :IPMI :SENSOR_INFO :SENSOR)))
+
+(defn state [] (map server-health (conf :hosts)))
+
+
