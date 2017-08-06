17747db5ae6335358ac30509759f3a926406e505
ronen <narkisr@gmail.com>
2017-08-06 15:36:25 +1000

    using delay for lazy config creation

--- project.clj
+++ project.clj
@@ -1,16 +1,21 @@
 (defproject colligere "0.1.0"
-  :description "Picking up metrics into riemann"
-  :url "TBD"
+  :description "IPMI metrics streamed into riemann"
+  :url "https://github.com/narkisr/colligere"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [
       [com.taoensso/timbre "4.7.4"]
-      [riemann-clojure-client "0.4.1" ]
+      [riemann-clojure-client "0.4.5" ]
       [org.clojure/data.xml "0.0.8"]
       [org.clojure/data.zip "0.1.2"]
       [org.clojure/clojure "1.8.0"]
       [http-kit "2.2.0"]]
-  
-  
+
+  :plugins [[jonase/eastwood "0.2.4"]
+            [lein-tag "0.1.0"]
+            [lein-ancient "0.6.7" :exclusions [org.clojure/clojure]]
+            [lein-set-version "0.3.0"] [lein-gorilla "0.4.0"]
+            [lein-cljfmt "0.5.6"]]
+
     :main colligere.core
     :aot  [colligere.core]
   )
--- src/colligere/core.clj
+++ src/colligere/core.clj
@@ -1,16 +1,16 @@
 (ns colligere.core
   (:gen-class true)
-  (:require 
-    [taoensso.timbre :as timbre :refer [debug  info  warn  error  fatal  report]]
-    [org.httpkit.timer :as timer]
-    [riemann.client :as r]
-    [clojure.string :refer (join lower-case replace)]
-    [clojure.data.xml :refer (parse-str)] 
-    [clojure.zip :refer (xml-zip)]
-    [org.httpkit.client :as http]
-    [clojure.data.zip.xml :refer  [xml-> xml1-> attr attr= text]]))
+  (:require
+   [taoensso.timbre :as timbre :refer [debug  info  warn  error  fatal  report]]
+   [org.httpkit.timer :as timer]
+   [riemann.client :as r]
+   [clojure.string :refer (join lower-case replace)]
+   [clojure.data.xml :refer (parse-str)]
+   [clojure.zip :refer (xml-zip)]
+   [org.httpkit.client :as http]
+   [clojure.data.zip.xml :refer  [xml-> xml1-> attr attr= text]]))
 
-(def conf (read-string  (slurp "config.edn")))
+(def conf (delay (read-string (slurp "config.edn"))))
 
 (defn login [host] (str "https://" host "/cgi/login.cgi"))
 
@@ -23,43 +23,43 @@
 (defn get-health [{:keys [host auth]}]
   (let [sid (get-sid host auth)
         headers {"Cookie" (str "SID=" sid ";" " mainpage=health; subpage=servh_sensor")
-                 "Accept" "application/json" }
+                 "Accept" "application/json"}
         form-data {"SENSOR_INFO.XML" "(1,ff)"}
-        {:keys [body status error]}  @(http/post (ipmi host) {:headers headers :form-params form-data :insecure? true}) ]
-    (if (or error (not (= status 200))) 
+        {:keys [body status error]}  @(http/post (ipmi host) {:headers headers :form-params form-data :insecure? true})]
+    (if (or error (not (= status 200)))
       (warn error)
       (xml-zip (parse-str body)))))
 
-(defn norm-name [m] 
+(defn norm-name [m]
   (-> m :NAME (replace " " "-") lower-case keyword))
 
 (defn server-health [host]
   (when-let [health (get-health host)]
     (map #(attr % (juxt norm-name :READING)) (xml-> health :IPMI :SENSOR_INFO :SENSOR))))
 
-(defn state [] 
-  (map #(some->> % server-health flatten (apply hash-map) (merge {:host (:host %)})) (conf :hosts)))
+(defn state []
+  (map #(some->> % server-health flatten (apply hash-map) (merge {:host (:host %)})) (@conf :hosts)))
 
-
-(defn client [] (r/tcp-client {:host (get-in conf [:riemann :host])}))
+(defn client [] (r/tcp-client {:host (get-in @conf [:riemann :host])}))
 
 (def memo-client (memoize client))
 
-(defn metric [m host] {:host host :service "ipmi" :state "running" :metric m :tags ["ipmi" "cpu" "temp"] :description "IPMI CPU temperature"})
+(defn metric [m host]
+  {:host host :service "ipmi" :state "running" :metric m :tags ["ipmi" "cpu" "temp"] :description "IPMI CPU temperature"})
 
 (defn send-metrics []
   (let [c (memo-client)]
-    (doseq [{:keys [host cpu-temp]} (state) 
-          :when host
-          :let [cpu-unhex (Integer/parseInt (replace cpu-temp "c000" "") 16) m (metric cpu-unhex host)] ]
-    (debug m)
-    (-> c (r/send-event m) (deref 5000 ::timeout)))))
+    (doseq [{:keys [host cpu-temp]} (state)
+            :when host
+            :let [cpu-unhex (Integer/parseInt (replace cpu-temp "c000" "") 16) m (metric cpu-unhex host)]]
+      (debug m)
+      (-> c (r/send-event m) (deref 5000 ::timeout)))))
 
 (defn schedule []
-   (loop [] 
-     (timer/schedule-task (conf :poll) (send-metrics))
-     (Thread/sleep (conf :poll)) 
-     (recur)))
+  (loop []
+    (timer/schedule-task (@conf :poll) (send-metrics))
+    (Thread/sleep (@conf :poll))
+    (recur)))
 
 (defn -main [& args]
   (schedule))
